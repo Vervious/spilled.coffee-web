74cf9cf19db754e55a04ffdfe3237ab35a6a0c6c
Benjamin Y Chan <me@vervious.com>
2018-05-14 15:53:39 -0400

    It's lit

--- cmds/src/bob.expert/lib/designer/simple.go
+++ cmds/src/bob.expert/lib/designer/simple.go
@@ -5,8 +5,25 @@ import (
 	"strconv"
 )
 
-func SimpleView(title string, b *bytes.Buffer) *bytes.Buffer {
+func SimpleView(title string, requestedGetVars map[string]string, requestedPostVars map[string]string, b *bytes.Buffer) *bytes.Buffer {
 	s := b.String()
+
+	// create text boxes
+	textBoxes := ""
+	if len(requestedGetVars) > 0 {
+		textBoxes += "<form method=\"get\">\n"
+		for k, v := range requestedGetVars {
+			textBoxes += k + ": <input type='text' name='" + k + "' value='" + v + "'><br>\n"
+		}
+		textBoxes += "<input type='submit' value='Submit'>\n</form>"
+	}
+	if len(requestedPostVars) > 0 {
+		textBoxes += "<form method=\"post\">\n"
+		for k, v := range requestedPostVars {
+			textBoxes += k + ": <input type='text' name='" + k + "' value='" + v + "'><br>\n"
+		}
+		textBoxes += "<input type='submit' value='Submit'>\n</form>"
+	}
 
 	// do some inefficient concatenation, for now
 	s = `
@@ -17,6 +34,9 @@ func SimpleView(title string, b *bytes.Buffer) *bytes.Buffer {
     <title>` + title + `</title>
 </head>
 <body>
+<p style="font-size: 1.5em; padding-left: 1.5em;">
+` + textBoxes + `
+</p>
 <p style="font-size: 1.5em; padding-left: 1.5em; border-left: 2px solid gray;">
 ` + s + `
 </p>
--- cmds/src/helloworld/main.go
+++ cmds/src/helloworld/main.go
@@ -14,9 +14,29 @@ func main() {
 
 	b := &buf
 
+	// key: variable name. Value: default value.
+	requestedGetVars := map[string]string{
+		"exampleget": "",
+	}
+	requestedPostVars := map[string]string{
+		"examplepost": "",
+	}
+
+	requestMethod := os.Getenv("REQUEST_METHOD")
+	switch requestMethod {
+	case "POST":
+		postData := os.Getenv("POST_DATA")
+		fmt.Fprintf(b, "post data: %v\n\n", postData)
+	case "GET":
+		queryString := os.Getenv("QUERY_STRING")
+		fmt.Fprintf(b, "query string: %v\n\n", queryString)
+	default:
+		fmt.Fprintf(b, "No input received.\n")
+	}
+
 	fmt.Fprintf(b, "Server time at %s is %s\n",
 		os.Getenv("SERVER_NAME"), time.Now().Format(time.RFC1123))
-	b = designer.SimpleView("helloworld", b)
+	b = designer.SimpleView("helloworld", requestedGetVars, requestedPostVars, b)
 	// fmt.Println("Content-type: text/html")
 	// fmt.Printf("Content-Length: %d\n\n", b.Len())
 	b.WriteTo(os.Stdout)
